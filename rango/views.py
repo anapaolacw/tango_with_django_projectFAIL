21343d26f772292435279a3403ae03c9352ca576
anapaolacw <apcw96@gmail.com>
2022-01-31 13:38:59 +0000

    Chapter 4, add templates

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    #return render(request, 'rango/about.html')
-    return HttpResponse('Rango says here is the about page.<br><a href=\'/rango/\'>Index</a>')
+    return render(request, 'rango/about.html')
